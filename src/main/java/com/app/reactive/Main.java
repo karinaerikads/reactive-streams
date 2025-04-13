22116009198d2af0bddb9a6a04897dec68002c80
karinaerikads <karinaerikads48@gmail.com>
2025-04-13 17:24:01 -0300

    adiciona caso de teste na main

--- src/main/java/com/app/reactive/Main.java
+++ src/main/java/com/app/reactive/Main.java
@@ -1,7 +1,15 @@
 package com.app.reactive;
 
+import org.reactivestreams.Subscriber;
+
 public class Main {
     public static void main(String[] args) {
+        YouTubeChannel channel = new YouTubeChannel();
+        channel.addVideo("Reactive programming with java");
+        channel.addVideo("Introduction to Machine Learning");
+        channel.addVideo("Web development basic");
 
+        User userSubscriber = new User("Karina");
+        channel.subscribe((Subscriber<? super String>) userSubscriber);
     }
 }
