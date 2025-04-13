44862218ed59b9ed28ff0e19276ecc60f491a26f
karinaerikads <karinaerikads48@gmail.com>
2025-04-13 15:00:58 -0300

    implementa métodos do subscription

--- src/main/java/com/app/reactive/YouTubeSubscription.java
+++ src/main/java/com/app/reactive/YouTubeSubscription.java
@@ -6,12 +6,33 @@ import org.reactivestreams.Subscription;
 import java.util.List;
 
 public class YouTubeSubscription implements Subscription {
+
+    private Subscriber<? super String> subscriber;
+
+    private List<String> videos;
+
     public YouTubeSubscription(Subscriber<? super String> subscriber, List<String> videos) {
+        this.subscriber = subscriber;
+        this.videos = videos;
     }
 
     @Override
-    public void request(long l) {
+    public void request(long size) {
+        System.out.println("request(unbounded)");
+        long requestNumberItems = getRequestNumberItems(size);
 
+        for (int i = 0; i < requestNumberItems; i++){
+            subscriber.onNext(videos.get(i));
+        }
+
+        subscriber.onComplete();
+    }
+
+    private long getRequestNumberItems(long size) {
+        if (size <= videos.size()){
+            return size;
+        }
+        return videos.size();
     }
 
     @Override
